983d5f5ddcad3d7639373a191292548f9b5e138c
Roshan Patel <ovicior@gmail.com>
2023-04-04 19:38:18 -0400

    image exporter started

--- PPMImageExporter.h
+++ PPMImageExporter.h
@@ -19,58 +19,25 @@ public:
     }
 
     void export(std::string path, int width, int height, int factor, vector<glm::vec3> data) {
-        string out = "";
+        std::string out = "";
         std::ofstream op;
-        std::string str;
-        int i,j;
-        int r,g,b;
-        int factor;
-
-        fp.open(filename.c_str());
-
-        if (!fp.is_open())
-            throw std::invalid_argument("File not found!");
-
-        std::cout << "Image file opened" << std::endl;
-
-        //read line by line and ignore comments
-        std::stringstream input;
-        std::string line;
-
-        while (std::getline(fp,line)) {
-            if ((line.length()>0) && (line[0]!='#')) {
-                input << line << std::endl;
-            }
-        }
-        fp.close();
-
 
         //read in the word P3
+        out += "P3" + "\n";
+        out += width + " " + height + "\n";
+        out += "255\n";
 
-        input >> str;
 
-
-        //now read in the width and height
-
-        input >> width >> height;
-
-        //read in the factor
-        input >> factor;
-
-        image = new GLubyte[3*width*height];
-
-        for (i=0;i<height;i++)
-        {
-            for (j=0;j<width;j++)
-            {
-                input >> r >> g >> b;
-                image[3*((height-1-i)*width+j)] = r;
-                image[3*((height-1-i)*width+j)+1] = g;
-                image[3*((height-1-i)*width+j)+2] = b;
-                // << "r=" << (int)r << ", g=" << (int)g << ",b=" << (int)b << endl;
-            }
-        }
-        //fp.close();
+//        for (i=0;i<height;i++)
+//        {
+//            for (j=0;j<width;j++)
+//            {
+//                input >> r >> g >> b;
+//                image[3*((height-1-i)*width+j)] = r;
+//                image[3*((height-1-i)*width+j)+1] = g;
+//                image[3*((height-1-i)*width+j)+2] = b;
+//            }
+//        }
     }
 
 };
